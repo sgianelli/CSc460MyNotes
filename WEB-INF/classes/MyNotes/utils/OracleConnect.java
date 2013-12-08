a0dcbb8825115f7bd69522e5f60f7c3ac858325b
sjdev <sjdev@github.com>
2013-12-07 21:00:46 -0700

    Initial Commit

---
+++ WEB-INF/classes/MyNotes/utils/OracleConnect.java
@@ -0,0 +1,11 @@
+package MyNotes.utils;
+
+public class OracleConnect
+{
+    public static final String connect_string = "jdbc:oracle:thin:@aloe.cs.arizona.edu:1521:oracle";
+    public static final String user_name = "shanejgianelli";
+    public static final String password = "a6080";
+}
+
+
+
