fd10bc91f907a7d9dafec7e07a0904f92da61949
Kate Maroney <katemaroney@on-campus-114-134.vpn.arizona.edu>
2013-12-07 22:48:06 -0700

    Fixing an exception

--- WEB-INF/classes/MyNotes/servlets/AddCard.java
+++ WEB-INF/classes/MyNotes/servlets/AddCard.java
@@ -160,7 +160,7 @@ public class AddCard extends HttpServlet
    }
 
 
-   public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException, 
+   public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException 
    {
       res.setContentType("text/html");
       PrintWriter out = res.getWriter();
